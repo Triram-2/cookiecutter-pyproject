a4d7706a8a7146b8f17f0461a6ebffa7e502b792
Misha <bobyyy239@gmail.com>
2025-08-29 18:06:33 +0300

    feat(hooks): realised base post_gen hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+print('Project created successfully!')
+print('Next steps:')
+print('1. cd {{cookiecutter.project_slug}}')
+print('2. git init')
+print('3. git add .')
+print('4. git commit -m "Initial commit"')
+
+# Удаление ненужного пользователю
+if '{{ cookiecutter.use_docker }}' != 'y':
+    shutil.rmtree('docker')
+if '{{ cookiecutter.use_k8s }}' != 'y' or '{{ cookiecutter.use_docker }}' != 'y':
+    shutil.rmtree('k8s')
+if '{{ cookiecutter.use_gitlab }}' != 'y':
+    shutil.rmtree('.gitlab')
+    os.remove('.gitlab-ci.yml')
+if '{{ cookiecutter.use_todo.md }}' != 'y':
+    os.remove('TODO.md')
+
+# Лицензия
+license_ = "{{ cookiecutter.license }}"
+if license_ in {"MIT", "BSD", "Apache", "GPL"}:
+    shutil.move('template_data/licenses/{{ cookiecutter.license }}', 'LICENSE')
+
+# Удаление лишнего
+shutil.rmtree('template_data')
