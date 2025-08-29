7dc29c92b1afad24af1f69e5712bd35c0ebc71c4
Misha <bobyyy239@gmail.com>
2025-08-29 21:40:48 +0300

    feat(__init__): realised

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -0,0 +1,10 @@
+import sys
+from pathlib import Path
+
+root = Path(__file__).resolve().parent.parent / {{ cookiecutter.project_slug }}
+sys.path.insert(0, str(root))
+
+
+__all__ = ['__version__']
+
+__version__ = "{{ cookiecutter.version }}"
